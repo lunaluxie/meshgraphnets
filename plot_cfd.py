98bac52dee839b172a3ef74b03e963af2ea7a241
Lux <luna@lunais.me>
2024-11-03 17:46:30 +0100

    optionally plot ground truth

--- plot_cfd.py
+++ plot_cfd.py
@@ -24,42 +24,50 @@ from matplotlib import tri as mtri
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("rollout_path", None, "Path to rollout pickle file")
+flags.DEFINE_bool("use_gt", False, "Whether to animate the ground truth")
 
 
 def main(unused_argv):
     with open(FLAGS.rollout_path, "rb") as fp:
         rollout_data = pickle.load(fp)
 
-    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection="3d")
     skip = 10
-    num_steps = rollout_data[0]["gt_velocity"].shape[0]
+    num_steps = rollout_data[0]["gt_pos"].shape[0]
     num_frames = len(rollout_data) * num_steps // skip
 
     # compute bounds
     bounds = []
     for trajectory in rollout_data:
-        bb_min = trajectory["gt_velocity"].min(axis=(0, 1))
-        bb_max = trajectory["gt_velocity"].max(axis=(0, 1))
+        bb_min = trajectory["gt_pos"].min(axis=(0, 1))
+        bb_max = trajectory["gt_pos"].max(axis=(0, 1))
         bounds.append((bb_min, bb_max))
 
     def animate(num):
         step = (num * skip) % num_steps
         traj = (num * skip) // num_steps
         ax.cla()
-        ax.set_aspect("equal")
-        ax.set_axis_off()
-        vmin, vmax = bounds[traj]
-        pos = rollout_data[traj]["mesh_pos"][step]
+        bound = bounds[traj]
+        ax.set_xlim([bound[0][0], bound[1][0]])
+        ax.set_ylim([bound[0][1], bound[1][1]])
+        ax.set_zlim([bound[0][2], bound[1][2]])
+
+        pos_key = "gt_pos" if FLAGS.use_gt else "pred_pos"
+        pos = rollout_data[traj][pos_key][step]  # pred_pos
         faces = rollout_data[traj]["faces"][step]
-        velocity = rollout_data[traj]["pred_velocity"][step]
-        triang = mtri.Triangulation(pos[:, 0], pos[:, 1], faces)
-        ax.tripcolor(triang, velocity[:, 0], vmin=vmin[0], vmax=vmax[0])
-        ax.triplot(triang, "ko-", ms=0.5, lw=0.3)
+        ax.plot_trisurf(pos[:, 0], pos[:, 1], faces, pos[:, 2], shade=True)
         ax.set_title("Trajectory %d Step %d" % (traj, step))
         return (fig,)
 
-    _ = animation.FuncAnimation(fig, animate, frames=num_frames, interval=100)
-    plt.show(block=True)
+    anim = animation.FuncAnimation(fig, animate, frames=num_frames, interval=100)
+    writervideo = animation.FFMpegWriter(fps=50 // skip)  # realtime using dt=0.02s
+    anim.save(
+        ".".join(
+            [FLAGS.rollout_path.split(".")[0], "gt" if FLAGS.use_gt else "pred", "mp4"]
+        ),
+        writer=writervideo,
+    )
 
 
 if __name__ == "__main__":
